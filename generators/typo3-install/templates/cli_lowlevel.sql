bde2b5222efd8e7119092d44cb3b6f39fcc7e43b
Joschi Kuphal <joschi@kuphal.net>
2016-12-04 23:04:47 +0100

    Fixes TYPO3 9 incompatibility when creating lowlevel CLI user (closes #27)

--- generators/typo3-install/templates/cli_lowlevel.sql
+++ generators/typo3-install/templates/cli_lowlevel.sql
@@ -1,2 +1,1 @@
-INSERT INTO `be_users` (`pid`, `username`, `password`, `admin`, `usergroup`, `disable`, `starttime`, `endtime`) VALUES
-	(0, '_cli_lowlevel', '$P$CKU97a28gDKJZRDukRKIfJmKTWoVmJ/', 0, '', 0, 0, 0);
+INSERT INTO `be_users` (`pid`, `username`, `password`, `admin`, `usergroup`, `disable`, `starttime`, `endtime`) VALUES (0, '_cli_lowlevel', '$P$CKU97a28gDKJZRDukRKIfJmKTWoVmJ/', 0, '', 0, 0, 0);
