0405ad6feccb8a4801b7bba3f3ce73cdcb00071d
Joschi Kuphal <joschi@kuphal.net>
2016-12-04 23:06:42 +0100

    Fixes TYPO3 8 database connection problem (closes #26)

--- generators/typo3-setup/templates/typo3conf/init.php
+++ generators/typo3-setup/templates/typo3conf/init.php
@@ -1,24 +1,35 @@
 <?php
 
 if (PHP_SAPI != 'cli') {
-	exit(1);
+    exit(1);
 }
 
 if (!@is_file(__DIR__.DIRECTORY_SEPARATOR.'LocalConfiguration.php')) {
-	exit(2);
+    exit(2);
 }
 
 if (!@is_file(__DIR__.DIRECTORY_SEPARATOR.'init.sql') || !strlen($sql = file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'init.sql'))) {
-	exit(3);
+    exit(3);
 }
 
 $error		= 0;
 $config		= include __DIR__.DIRECTORY_SEPARATOR.'LocalConfiguration.php';
-$db			= new \PDO('mysql:'.$config['DB']['host'].'=localhost;dbname='.$config['DB']['database'], $config['DB']['username'], $config['DB']['password']);
+$dbParams   = $config['DB'];
+
+// >= TYPO3 8
+if (!empty($dbParams['Connections']) && !empty($dbParams['Connections']['Default'])) {
+    $dbParams = $dbParams['Connections']['Default'];
+    $db			= new \PDO('mysql:'.$dbParams['host'].'=localhost;dbname='.$dbParams['dbname'], $dbParams['user'], $dbParams['password']);
+
+// < TYPO3 8
+} else {
+    $db			= new \PDO('mysql:'.$dbParams['host'].'=localhost;dbname='.$dbParams['database'], $dbParams['username'], $dbParams['password']);
+}
+
 if ($db instanceof \PDO) {
-	if (!($db->query($sql) instanceof \PDOStatement)) {
-		$error		= 4;
-	}
+    if (!($db->query($sql) instanceof \PDOStatement)) {
+        $error		= 4;
+    }
 }
 
 @unlink(__FILE__);
